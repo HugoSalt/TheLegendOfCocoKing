6f5184f13111f394a925cd430f33daf528423834
HugoSalt <hugosalt@protonmail.com>
2018-05-02 05:35:20 +0200

    Landscape Done

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+	private float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		// Y axis (vertical) oscillation because of ocean waves
+		//this.MovePosition.Translate(new Vector3(0, 1.0f, 0) * Mathf.Sin(Mathf.PI * Time.deltaTime));	
+	}
+}
