a671b6dc6db4babc64c946e335509ac25f9ef387
HugoSalt <hugosalt@protonmail.com>
2018-05-03 03:36:36 +0200

    Changed Wheel to use Grab

---
+++ Assets/Scripts/Ship/Ship.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+	private Rigidbody shipRigidBody;
+	public float forwardForce;
+	public float oscillationAmp;
+	public float oscillationFreq;
+	private Rigidbody wheelRigidBody;
+	public float wheelMultiplier;
+
+	// Use this for initialization
+	void Start () {
+		wheelRigidBody = transform.Find("Ship Wheel").
+				transform.Find("Wheel").GetComponent<Rigidbody>();
+		shipRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		// Y axis (vertical) oscillation because of ocean waves
+		transform.Translate( Vector3.up
+				* oscillationAmp * Mathf.Sin(2*Mathf.PI*oscillationFreq*Time.time) );
+		// Constant forward force
+		shipRigidBody.AddForce(forwardForce * transform.forward);
+		// Get wheel rotation speed
+		float angVelocity = wheelRigidBody.angularVelocity.z;
+		//	print(angVelocity);
+		// Apply torque to boat
+		shipRigidBody.AddTorque(angVelocity * wheelMultiplier * transform.up);
+	}
+}
---
+++ Assets/Scripts/Ship/WheelHandle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelHandle : MonoBehaviour {
+
+	private Vector3 lastGrabPos;
+	private Rigidbody wheelRigidBody;
+	public float wheelGrabForceMultiply;
+	// Use this for initialization
+	void Start () {
+		wheelRigidBody = transform.parent.parent.gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerStay(Collider other) {
+		print("heloo");
+		if (other.gameObject.tag == "HandManipulator"){
+
+			if (other.gameObject.GetComponent<HandInteractor>().IsGrabbing) {
+
+				Vector3 currentGrabPos = other.gameObject.transform.position ;
+				Vector3 handleCenter = transform.position;
+				// Apply Force on wheel (ship will read on its own
+				// the wheel's torque velocity)
+				wheelRigidBody.AddForceAtPosition(handleCenter,(handleCenter-currentGrabPos)*wheelGrabForceMultiply);
+				Debug.DrawLine(currentGrabPos, handleCenter, Color.red);
+
+			}
+
+		}
+		
+	}
+
+}
