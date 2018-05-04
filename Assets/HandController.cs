b3bd03c89b347fc1670b3b9289c6106754b1c72b
unknown <TP@IC-IIG-DPC-02.intranet.epfl.ch>
2018-05-04 17:18:58 +0200

    Values adjusted for VR + Hand Controllers

---
+++ Assets/HandController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour {
+
+    private SteamVR_TrackedController device;
+    private HandInteractor handInteractorScript;
+    public HandInteractor handInteractorObject;
+
+	// Use this for initialization
+	void Start () {
+        device = GetComponent<SteamVR_TrackedController>();
+        device.TriggerClicked += TriggerClick;
+        device.TriggerUnclicked += TriggerUnclick;
+        handInteractorScript = handInteractorObject.GetComponent<HandInteractor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TriggerClick(object sender, ClickedEventArgs e) {
+        handInteractorScript.IsGrabbing = true;
+    }
+
+    void TriggerUnclick(object sender, ClickedEventArgs e)
+    {
+        handInteractorScript.IsGrabbing = false;
+    }
+}
