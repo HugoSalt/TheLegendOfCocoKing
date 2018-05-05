96ccf93304b8a979e4b37567b52c3b28c335283d
HugoSalt <hugosalt@protonmail.com>
2018-05-06 01:06:20 +0200

    Organizing scripts and cannonball splash effects

---
+++ Assets/Scripts/Ship/CannonBall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour {
+
+	public GameObject splashEffect;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other) {
+
+		if (other.gameObject.tag == "OCEAN_COLLIDER"){
+			// Emit a "splash" particle effect
+			GameObject splashParticles = (GameObject) Instantiate(splashEffect, 
+                               transform.position,  Quaternion.Euler(new Vector3(90, 0, 0)));
+			splashParticles.transform.localScale = new Vector3(2,2,2);
+		}
+		
+	}
+}
