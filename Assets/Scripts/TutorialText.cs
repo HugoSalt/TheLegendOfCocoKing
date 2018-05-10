016a88727f821de91977f880cdc9bdce1cd4a310
Akynna <tim.nguyen@epfl.ch>
2018-05-10 20:15:58 +0200

    Welcome Text prefab

---
+++ Assets/Scripts/TutorialText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialText : MonoBehaviour {
+
+	private TextMesh textMesh;
+	private float alpha;
+	public float timer;
+
+	void Start () {
+		textMesh = GetComponent<TextMesh>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		timer -= Time.deltaTime;
+		if(timer < 0) {
+			textMesh.color = new Color(1, 1, 1, alpha);
+			alpha -= 0.01f;
+		}
+		
+		if(alpha < 0) {
+			Destroy(this.gameObject);
+		}
+	}
+}
