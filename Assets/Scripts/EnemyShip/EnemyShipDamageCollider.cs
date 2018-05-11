9306dd9e697733569a95e9a34b7ed4e43bdbe2ab
Mike Bardet <mike.bardet@epfl.ch>
2018-05-11 19:19:10 +0200

    Add enemy ship scripts

---
+++ Assets/Scripts/EnemyShip/EnemyShipDamageCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShipDamageCollider : MonoBehaviour {
+
+	private EnemyShip shipScript;
+	// Use this for initialization
+	void Start () {
+		shipScript = transform.parent.gameObject.GetComponent<EnemyShip>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "DEADLY_COLLIDER") {
+			shipScript.TakeDamage();
+		}	
+	}
+}
