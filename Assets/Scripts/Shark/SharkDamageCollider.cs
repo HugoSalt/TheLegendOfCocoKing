20500332ee0ce36d00cb60b0fff6cf0adb5c8fe4
Akynna <tim.nguyen@epfl.ch>
2018-05-15 00:51:43 +0200

    Added Shark and Harpoon objects, prefabs and scripts

---
+++ Assets/Scripts/Shark/SharkDamageCollider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkDamageCollider : MonoBehaviour {
+
+	private Shark sharkScript;
+
+	// Use this for initialization
+	void Start () {
+		sharkScript = transform.parent.gameObject.GetComponent<Shark>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "DEADLY_COLLIDER") {
+			sharkScript.TakeDamage();
+		}	
+	}
+}
